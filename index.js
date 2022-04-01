de42a281bbee49f0f61450df94792864b139adce
Giovanna <giovannagmoeller@gmail.com>
2022-04-01 16:44:30 -0300

    Revert "Responsive"

--- index.js
+++
@@ -1,9 +0,0 @@
-/* Esse arquivo foi criado para manipular o menu responsivo */
-
-const menu = document.querySelector('.menu')
-const menuIcon = document.querySelector('.sideMenu img')
-
-menuIcon.addEventListener('click', function() {
-  // quando houver clique no ícone de menu
-  menu.classList.toggle('active')
-})
